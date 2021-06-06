0b1bf50db3da0040849aaf2a596a8e757e2d590d
chencong <chencong260@gmail>
2021-06-06 17:44:35 +0900

    2.2 Processing form submission

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -7,11 +7,13 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient.Type;
 
+@Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
@@ -39,6 +41,12 @@ public class DesignTacoController {
 		model.addAttribute("design", new Taco());
 		return "design";
 		
+	}
+	
+	@PostMapping
+	public String processDesgin(Taco design) {
+		log.info("Processing design : " + design);
+		return "redirect:/orders/current";
 	}
 
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
