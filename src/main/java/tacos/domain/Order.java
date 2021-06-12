31f127003172df52ec6e1caa135251bd92b01eb5
chencong <chencong260@gmail>
2021-06-12 18:05:15 +0900

    3 working with data
    spring jpa practice

--- src/main/java/tacos/domain/Order.java
+++ src/main/java/tacos/domain/Order.java
@@ -1,5 +1,17 @@
 package tacos.domain;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -9,22 +21,57 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "Taco_Order")
 public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToMany(targetEntity = Taco.class)
+	private List<Taco> tacos = new ArrayList<>();
 	
 	@NotBlank(message = "Name is required")
-	private String name;
+	@Column(name = "deliveryName")
+	private String customerName;
+	
 	@NotBlank(message = "Street is required")
+	@Column(name = "deliveryStreet")
 	private String street;
+	
 	@NotBlank(message = "City is required")
+	@Column(name = "deliveryCity")
 	private String city;
+	
 	@NotBlank(message = "State is required")
+	@Column(name = "deliveryState")
 	private String state;
+	
 	@NotBlank(message = "Zip is required")
+	@Column(name = "deliveryZip")
 	private String zip;
+	
 	@CreditCardNumber(message = "Not a valid credit card number")
 	private String ccNumber;
+	
 	@Pattern(regexp = "^(0[1-9]|1[0-2])([\\\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
 	private String ccExpiration;
+	
 	@Digits(integer = 3, fraction = 0,message = "Invalid CVV")
 	private String ccCVV;
+	
+	private Date placedAt;
+	
+	@PrePersist
+	void placedAt() {
+		this.placedAt = new Date();
+	}
+	
+	public void addTaco(Taco taco) {
+		this.tacos.add(taco);
+	}
+
+	public void addDesign(Taco taco) {
+		tacos.add(taco);
+	}
 }
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,32 +2,48 @@ package tacos.web;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
+import tacos.data.OrderRepository;
 import tacos.domain.Order;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
 	
+	private final OrderRepository orderRepo;
+	
+	@Autowired
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+
 	@GetMapping("/current")
 	public String orderForm(Order order) {
 		return "orderForm";
 	}
 	
 	@PostMapping
-	public String processOrder(@Valid Order order, Errors errors) {
+	public String processOrder(@Valid Order order, Errors errors, SessionStatus ssSessionStatus) {
 		if(errors.hasErrors()) {
 			return "orderForm";
 		}
 		
 		log.info("Processiong order : " + order);
+		
+		orderRepo.save(order);
+		
+		ssSessionStatus.setComplete();
 		return "redirect:/";
 	}
 }
